e7cf9b415ca9a3ced93bda3abdfc9fa94529c64c
Giovanni <gio.sales95@gmail.com>
2026-06-26 10:04:25 -0300

    testing ways to print log messages

--- Source/Slash/Private/Items/Item.cpp
+++ Source/Slash/Private/Items/Item.cpp
@@ -12,10 +12,22 @@ void AItem::BeginPlay()
 	Super::BeginPlay();
 
 	UE_LOG(LogTemp, Warning, TEXT("BEGIN PLAY CALLED!"));
+
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Blue, FString("Item OnScreenMessage!"));
+	}
 }
 void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
+	// UE_LOG(LogTemp, Warning, TEXT("Dt: %f"), DeltaTime);
+	//
+	// if (GEngine)
+	// {
+	// 	FString name = GetName();
+	// 	FString msg = FString::Printf(TEXT("Dt: %f, item name: %s"), DeltaTime, *name);
+	// 	GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Blue, msg);
+	// }
 }
 
